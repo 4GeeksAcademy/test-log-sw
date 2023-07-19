58de8c77b09a437a7270ad909ee80de87087f060
ValentinFrAr <116929850+ValentinFrAr@users.noreply.github.com>
2023-07-19 18:44:32 +0000

    done

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -28,7 +28,7 @@ export const Navbar = () => {
           <Link to="/" className="navbar-brand">
             <img
               className="logo-nav"
-              src="https://i.pinimg.com/474x/a2/a2/27/a2a227afa5d96e329085b989357b1129--star-wars-action-figures-episode-iv.jpg"
+              src="https://logodownload.org/wp-content/uploads/2015/12/star-wars-logo-3-1.png"
               alt=""
             />
           </Link>
@@ -43,40 +43,40 @@ export const Navbar = () => {
           >
             <span className="navbar-toggler-icon"></span>
           </button>
-          <div className="btn-group dropstart">
-            <button
-              type="button"
-              className="btn btn-favorite dropdown-toggle"
-              data-bs-toggle="dropdown"
-              aria-expanded="false"
-            >
-              FAVORITES
-              <span className="fav-num">{store.favorites.length}</span>
-            </button>
-            <ul className="dropdown-menu  mx-auto">
-              {store.favorites.map((favorite, index) => {
-                return (
-                  <li key={index}>
-                    <h2>
-                      {favorite.name}{" "}
-                      <box-icon
-                        type="solid"
-                        className="bx-lg bx-tada-hover me-4"
-                        color="yellow"
-                        name="trash-alt"
-                        onClick={(event) => {
-                          event.stopPropagation();
-                          actions.deleteFavorite(favorite);
-                        }}
-                      ></box-icon>
-                    </h2>
-                  </li>
-                );
-              })}
-            </ul>
-          </div>
           {isLoggedIn ? (
             <>
+              <div className="btn-group dropstart">
+                <button
+                  type="button"
+                  className="btn btn-favorite dropdown-toggle"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  FAVORITES
+                  <span className="fav-num">{store.favorites.length}</span>
+                </button>
+                <ul className="dropdown-menu  mx-auto">
+                  {store.favorites.map((favorite, index) => {
+                    return (
+                      <li key={index}>
+                        <h2>
+                          {favorite.name}{" "}
+                          <box-icon
+                            type="solid"
+                            className="bx-lg bx-tada-hover me-4"
+                            color="yellow"
+                            name="trash-alt"
+                            onClick={(event) => {
+                              event.stopPropagation();
+                              actions.deleteFavorite(favorite);
+                            }}
+                          ></box-icon>
+                        </h2>
+                      </li>
+                    );
+                  })}
+                </ul>
+              </div>
               <h3>Hi, {store.user.email}</h3>
               <button
                 className="btn btn-primary btn-out  "
@@ -88,12 +88,12 @@ export const Navbar = () => {
           ) : (
             <div className="d-flex">
               <Link to="/login">
-                <button className="btn btn-primary btn-log btn-more me-3">
+                <button className="btn btn-primary  btn-favorite me-3">
                   Log In
                 </button>
               </Link>
               <Link to="/signup">
-                <button className="btn btn-primary btn-log btn-more">
+                <button className="btn btn-primary btn-favorite">
                   Sign Up
                 </button>
               </Link>
